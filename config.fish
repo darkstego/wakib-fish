1765eceedcd6c01dcc4121207eee2ad7a18eb04b
Abdulla Bubshait <darkstego@gmail.com>
2021-12-14 18:54:24 -0500

    Add extra shortcuts

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+	# Movement
 	bind \ei up-or-search
 	bind \ek down-or-search
 	bind \ej backward-char
@@ -7,6 +8,7 @@ if status is-interactive
 	bind \eo forward-word
 	bind \eU beginning-of-line
 	bind \eO end-of-line
+	# Delete
 	bind \ed backward-delete-char
 	bind \ef delete-char
 	bind \ee backward-kill-word
@@ -14,4 +16,13 @@ if status is-interactive
 	bind \eE backward-kill-line
 	bind \eR kill-line
 	bind \ew kill-whole-line
+	# Help
+	bind \ch __fish_man_page
+	bind \eh __fish_whatis_current_token
+	# Signals
+	bind \cg cancel-commandline
+	# Directories
+	bind \em __fish_list-current_token
+	# copy/paste
+	bind \cc fish_clipboard_copy
 end
