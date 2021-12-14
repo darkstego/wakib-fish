1009a56daadde77e64f83508c1aca2c4b2daa40b
Abdulla Bubshait <darkstego@gmail.com>
2021-12-14 16:38:47 -0500

    Add simple bindings

---
+++ config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+	bind \ei up-or-search
+	bind \ek down-or-search
+	bind \ej backward-char
+	bind \el forward-char
+	bind \eu backward-word
+	bind \eo forward-word
+	bind \eU beginning-of-line
+	bind \eO end-of-line
+	bind \ed backward-delete-char
+	bind \ef delete-char
+	bind \ee backward-kill-word
+	bind \er kill-word
+	bind \eE backward-kill-line
+	bind \eR kill-line
+	bind \ew kill-whole-line
+end
